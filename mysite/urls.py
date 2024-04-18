9be330e1f678a5668305221d9c8bce1af5a3e2ed
mobinazn <mobinazarenejad@gmail.com>
2024-04-18 14:31:03 +0330

    created first app
    created my first app named website which has 3 views for home, about and contact pages

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+# from website.views import http_test, json_test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path ('url address', 'view')
+    # path('http-test', http_test),
+    # path('json-test', json_test)
+
+    # path('website/', include('website.urls'))
+    path('', include('website.urls'))
 ]
