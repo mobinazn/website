b586fb82f06896a4e6b7ec0aa591b97eaacf6e45
mobinazn <mobinazarenejad@gmail.com>
2024-06-21 23:05:28 +0330

    makemigrations and migrate and database
    makemigrations and migrate wesite contact table and blog post table

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    # image
+    # author
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    # tag
+    # category
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self) -> str:
+        return '{} - {}'.format(self.title,self.id)
+
+
